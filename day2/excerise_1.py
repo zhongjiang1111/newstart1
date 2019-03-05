daf257861ac183d0689defc7b6664a871758d047
zhongjiang1111 <zhongjiang11111@gmail.com>
2019-03-05 22:07:40 +0800

    第2课练习

---
+++ day2/excerise_1.py
@@ -0,0 +1,15 @@
+# 练习，要求如下：
+#   1 循环验证用户输入的用户名与密码
+#   2 认证通过后，运行用户重复执行命令
+#   3 当用户输入命令为quit时，则退出整个程序
+
+name=input('用户名')
+password=input('密码')
+
+while name!='quit':
+    name = input('用户名')
+    password = input('密码')
+# 我这里没有理解题目
+
+#不理解tag啥意思
+
